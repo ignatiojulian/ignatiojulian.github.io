5ee23cc867656f97d1c39d80e8754a2b667d6714
Ignatio <ignatio.julian@ti.ukdw.ac.id>
2020-07-12 11:08:37 +0700

    [ENHANCEMENT] add file portfolio

---
+++ build/precache-manifest.ad6946a0d3c0dfe79fa17f6f9d6af1bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf387b35825eb63f082bd02dfb6dbc1",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a1d4357a081f7c83429f",
+    "url": "/react-portfolio/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "d813c9799fb22856389b",
+    "url": "/react-portfolio/static/js/2.9c0176f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.9c0176f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d4357a081f7c83429f",
+    "url": "/react-portfolio/static/js/main.41825db9.chunk.js"
+  },
+  {
+    "revision": "b102c8707bb102eb9eb6",
+    "url": "/react-portfolio/static/js/runtime-main.051e22e9.js"
+  }
+]);
---
+++ src/resumeData.js
@@ -0,0 +1,112 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Ignatio Julian",
+    "role": "Software Engineer",
+    "linkedinId":"Ignatio Julian",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I like new things, more interested in the frontend development and like new technology. like music and coffee time when free time",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/ignatio-julian-tara-02264a172/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/ignatiojulian",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://join.skype.com/invite/eAWYe3ysLHqf",
+          "className":"fa fa-skype"
+        }
+      ],
+    "aboutme":"I am currently a senior at the Duta Wacana Christian University in Yogyakarta and am pursuing a computer degree. I am currently exploring mobile frameworks such as Google Flutter and web frameworks such as ember.js and react.js and am also interested in machine learning. I dedicate myself to the development of technology for the future",
+    "address":"Yogyakarta, Indonesia",
+    "website":"ignatio.julian@ti.ukdw.ac.id",
+    "education":[
+      {
+        "UniversityName":"Duta Wacana Christian University Yogyakarta",
+        "specialization":"College Student",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2020",
+        "Achievements":"Himpunan Mahasiswa Teknik Informatika - 2017"
+      },
+      {
+        "UniversityName":"Seminary St. Peter Canisius Mertoyudan",
+        "specialization":"Student",
+        "MonthOfPassing":"Jul",
+        "YearOfPassing":"2016",
+        "Achievements":"Canisii Seminarium Orchestra, Sales & Marketing of Aquila Magazine"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"W3Ocean Pte Ltd",
+        "specialization":"Mobile Application Developer",
+        "MonthOfLeaving":"Aug 2019 - Mar",
+        "YearOfLeaving":"2019",
+        "Achievements":"ZeusX Mobile Application"
+      },
+      {
+        "CompanyName":"",
+        "specialization":"Web Application Developer",
+        "MonthOfLeaving":"Mar 2020 - ",
+        "YearOfLeaving":"Present",
+        "Achievements":""
+      }
+    ],
+    "skillsDescription":"Here are my skills that I can do",
+    "skills":[
+      {
+        "skillname":"Mobile Application Development",
+      },
+      {
+        "skillname":"Web Application Development"
+      },
+      {
+        "skillname":"Web Design"
+      },
+      {
+        "skillname":"Flutter"
+      },
+      {
+        "skillname":"PHP"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"Diet Identification",
+        "description":"Web App",
+        "imgurl":"images/portfolio/Picture1.png"
+      },
+      {
+        "name":"E-Commerce for Gamers",
+        "description":"Mobile App",
+        "imgurl":"images/portfolio/Picture2.png"
+      },
+      // {
+      //   "name":"project3",
+      //   "description":"mobileapp",  
+      //   "imgurl":"images/portfolio/project2.png"
+      // },
+      // {
+      //   "name":"project4",
+      //   "description":"mobileapp",
+      //   "imgurl":"images/portfolio/phone.jpg"
+      // }
+    ],
+    "Quotes":[
+      {
+        "description":"Good enough is never good enough, when extraordinary is expected and is the standard",
+        "name":"Ty Howard"
+      },
+      {
+        "description":"If you have a dream, chase it no matter what it takes.",
+        "name":"James Harden"
+      }
+    ]
+  }
+  
+  export default resumeData
